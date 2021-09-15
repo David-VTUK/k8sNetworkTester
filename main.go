9e97b4939e759c2c395b12134f9d8b196bbcab19
david <david.holder@gmail.com>
2021-09-15 20:34:43 +0100

    Init

--- main.go
+++ main.go
@@ -3,61 +3,118 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
-	"path/filepath"
-	"time"
-
-	"k8s.io/apimachinery/pkg/api/errors"
+	log "github.com/sirupsen/logrus"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/homedir"
+	"os"
+	"path"
+	"strings"
+)
+
+// Channel buffer size
+const (
+	defaultKubeconfig = "~/.kube/config"
+	burst             = 50
+	qps               = 25
 )
 
 func main() {
-	var kubeconfig *string
-	if home := homedir.HomeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	}
-	flag.Parse()
-
-	// use the current context in kubeconfig
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	kubeconfig, err := getConfig()
 	if err != nil {
-		panic(err.Error())
+		handleError(err)
 	}
 
-	// create the clientset
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		handleError(err)
+	}
+
+	// Increase the Burst and QOS values
+	config.Burst = burst
+	config.QPS = qps
+
+	// Build client from  config
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		panic(err.Error())
+		handleError(err)
 	}
-	for {
-		pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+
+	ctx := context.Background()
+
+	// numberOfContainers, err := getTotalNumberOfContainers(ctx, clientset)
+
+	if err != nil {
+		handleError(err)
+	}
+
+	getTotalNumberOfNodes(ctx, clientset)
+
+}
+
+func getTotalNumberOfNodes(ctx context.Context, clientset *kubernetes.Clientset) {
+	nodes, err := clientset.CoreV1().Nodes().List(ctx, metav1.ListOptions{})
+
+	if err != nil {
+		handleError(err)
+	}
+
+	for _, node := range nodes.Items {
+		if node.Labels["node-role.kubernetes.io/worker"] == "true" {
+			log.Infof("Found Worker")
+		}
+	}
+}
+
+/*
+func getTotalNumberOfContainers(ctx context.Context, clientSet *kubernetes.Clientset) (int, error) {
+
+	numberofcontainers := 0
+	pods, err := clientSet.CoreV1().Pods("").List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return 0, err
+	}
+
+	for _, pod := range pods.Items {
+		numberofcontainers += len(pod.Spec.Containers)
+	}
+	return numberofcontainers, nil
+}
+*/
+
+func getConfig() (string, error) {
+	var filename string
+	var err error
+	kubeconfigFlag := flag.String("kubeconfig", "", "path to the kubeconfig file")
+	flag.Parse()
+
+	filename = *kubeconfigFlag
+	if filename == "" {
+		filename = defaultKubeconfig
+	}
+	filename, err = homeDir(filename)
+	if err != nil {
+		return "", err
+	}
+	if _, err := os.Stat(filename); err != nil {
+		return "", err
+	}
+
+	return filename, nil
+}
+
+func homeDir(filename string) (string, error) {
+	if strings.Contains(filename, "~/") {
+		homedir, err := os.UserHomeDir()
 		if err != nil {
-			panic(err.Error())
+			return "", err
 		}
-		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
-
-		// Examples for error handling:
-		// - Use helper functions like e.g. errors.IsNotFound()
-		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-		namespace := "default"
-		pod := "example-xxxxx"
-		_, err = clientset.CoreV1().Pods(namespace).Get(context.TODO(), pod, metav1.GetOptions{})
-		if errors.IsNotFound(err) {
-			fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
-		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-			fmt.Printf("Error getting pod %s in namespace %s: %v\n",
-				pod, namespace, statusError.ErrStatus.Message)
-		} else if err != nil {
-			panic(err.Error())
-		} else {
-			fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
-		}
-
-		time.Sleep(10 * time.Second)
+		filename = strings.Replace(filename, "~/", "", 1)
+		filename = path.Join(homedir, filename)
 	}
+	return filename, nil
+}
+
+func handleError(err error) {
+	panic(err.Error())
 }
