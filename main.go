a4f9cd5a014c1cc57b79115715ea8afdf1a94df4
david <david.holder@gmail.com>
2021-09-16 19:55:22 +0100

    Update

--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"context"
 	"flag"
 	log "github.com/sirupsen/logrus"
+	appsv1 "k8s.io/api/apps/v1"
+	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"os"
 	"path"
 	"strings"
+	"time"
 )
 
 // Channel buffer size
@@ -18,6 +21,8 @@ const (
 	burst             = 50
 	qps               = 25
 )
+
+var nodeList []string
 
 func main() {
 	kubeconfig, err := getConfig()
@@ -49,6 +54,7 @@ func main() {
 	}
 
 	getTotalNumberOfNodes(ctx, clientset)
+	generateWorkloads(ctx, clientset)
 
 }
 
@@ -61,26 +67,58 @@ func getTotalNumberOfNodes(ctx context.Context, clientset *kubernetes.Clientset)
 
 	for _, node := range nodes.Items {
 		if node.Labels["node-role.kubernetes.io/worker"] == "true" {
-			log.Infof("Found Worker")
+			log.Infof("Found Worker %s", node.Name)
+			nodeList = append(nodeList, node.Name)
 		}
 	}
 }
 
-/*
-func getTotalNumberOfContainers(ctx context.Context, clientSet *kubernetes.Clientset) (int, error) {
+func generateWorkloads(ctx context.Context, clientset *kubernetes.Clientset) {
+	daemonset := &appsv1.DaemonSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "connectivity-checker",
+		},
+		Spec: appsv1.DaemonSetSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{"app": "connectivity-checker"},
+			},
+			Template: apiv1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{"app": "connectivity-checker"},
+				},
+				Spec: apiv1.PodSpec{
+					Containers: []apiv1.Container{
+						{
+							Name:  "nginx",
+							Image: "nginx:1.21.3",
+							Ports: []apiv1.ContainerPort{
+								{
+									Name:          "http",
+									Protocol:      apiv1.ProtocolTCP,
+									ContainerPort: 80,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	daemonsetClient := clientset.AppsV1().DaemonSets(apiv1.NamespaceDefault)
 
-	numberofcontainers := 0
-	pods, err := clientSet.CoreV1().Pods("").List(ctx, metav1.ListOptions{})
+	log.Info("Creating Daemonset")
+	result, err := daemonsetClient.Create(context.TODO(), daemonset, metav1.CreateOptions{})
 	if err != nil {
-		return 0, err
+		handleError(err)
 	}
 
-	for _, pod := range pods.Items {
-		numberofcontainers += len(pod.Spec.Containers)
+	log.Infof("Created Daemonset %s", result.Name)
+
+	for i := 0; i < 30; i++ {
+		log.Info(daemonset.Status)
+		time.Sleep(2 * time.Second)
 	}
-	return numberofcontainers, nil
 }
-*/
 
 func getConfig() (string, error) {
 	var filename string
